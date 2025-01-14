f83b3d65df29e355fc6e0b3173ea4333157141d2
Ajemark <ajemark0110@gmail.com>
2025-01-14 09:54:27 +0100

    Merging near, adding game 01

--- src/app/components/WalletConnector.tsx
+++ src/app/components/WalletConnector.tsx
@@ -1,30 +1,30 @@
-import { useNearWallet } from "../hooks/useNearWallet";
-import { WALLET_CONFIG } from "../config/wallet";
+// import { useNearWallet } from "../hooks/useNearWallet";
+// import { WALLET_CONFIG } from "../config/wallet";
 
-export function WalletConnector() {
-  const {
-    connectWallet,
-    disconnectWallet,
-    connected,
-    accountId,
-    loading,
-    error,
-  } = useNearWallet();
+// export function WalletConnector() {
+//   const {
+//     connectWallet,
+//     disconnectWallet,
+//     connected,
+//     accountId,
+//     loading,
+//     error,
+//   } = useNearWallet();
 
-  if (loading) {
-    return <div>Loading...</div>;
-  }
+//   if (loading) {
+//     return <div>Loading...</div>;
+//   }
 
-  if (error) {
-    return <div>Error: {error.message}</div>;
-  }
+//   if (error) {
+//     return <div>Error: {error.message}</div>;
+//   }
 
-  return (
-    <button
-      onClick={connected ? disconnectWallet : connectWallet}
-      className="wallet-button"
-    >
-      {connected ? `Disconnect ${accountId}` : "Connect NEAR"}
-    </button>
-  );
-}
+//   return (
+//     <button
+//       onClick={connected ? disconnectWallet : connectWallet}
+//       className="wallet-button"
+//     >
+//       {connected ? `Disconnect ${accountId}` : "Connect NEAR"}
+//     </button>
+//   );
+// }
